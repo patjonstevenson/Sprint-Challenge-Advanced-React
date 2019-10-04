0395201d00ad83f672838a38c2d8eb7bb7bd2c8a
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-04 09:36:33 -0700

    set up call to server, created array of team objects with properties country and players (an array of that country's players)

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
+import axios from "axios";
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dataByTeam: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(res => {
+        const data = res.data;
+        const teams = data
+          .map(player => player.country)
+          .reduce((acc, team) => acc.includes(team)
+            ? acc
+            : [...acc, team], [])
+        this.setState({
+          dataByTeam: teams
+            .map(team => {
+              return {
+                country: team,
+                players: data
+                  .filter(player => player.country === team)
+              };
+            })
+        });
+
+      })
+      .catch(err => console.log("Error getting data:\n", err));
+  }
+
+  componentDidUpdate() {
+    console.log("cDU running");
+
+
+    console.log("Data By Team:\n", this.state.dataByTeam);
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
 
 export default App;
