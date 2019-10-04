93f84d848f12e0b42abdeee9e061ab22a2eb2a43
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-04 10:39:25 -0700

    added totalSearches function

--- client/src/components/Team.js
+++ client/src/components/Team.js
@@ -1,17 +1,21 @@
 import React from "react";
 import Player from "./Player";
 
+export const totalSearches = team => team.players
+    .reduce((sum, player) => sum + player.searches, 0);
+
 const Team = ({ team }) => {
     return (
         <div className="team" id={team.country}>
             <div className="team-heading">
                 <h2>{team.country}</h2>
+
                 <a className="link" href="#">[back to top]</a>
             </div>
-
+            <p>Total searches: {totalSearches(team)}</p>
             <div className="players">
                 {team.players.map(player => (
-                    <Player player={player} />
+                    <Player key={player.id} player={player} />
                 ))}
             </div>
         </div>
