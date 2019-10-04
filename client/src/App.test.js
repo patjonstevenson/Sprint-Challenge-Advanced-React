0b60ec3d00ef847f63087af50f0d2e475a04c785
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-04 10:46:20 -0700

    set up unit test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "@testing-library/react";
 import App from './App';
+import { totalSearches } from "./components/Team";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders without crashing', () => {
+  render(<App />);
+});
+
+test("totalSearches sums the total searches", () => {
+  const team = {
+    players: [
+      { searches: 3 },
+      { searches: 2 },
+      { searches: 1 }
+    ]
+  };
+  const expected = 6;
+  expect(totalSearches(team)).toBe(expected);
 });
