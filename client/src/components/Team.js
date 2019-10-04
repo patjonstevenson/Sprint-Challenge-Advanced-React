2cea68fda0a142fad16099c488476c473513e35f
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-04 10:55:49 -0700

    MVP: added useLocalStorage custom hook and used in Team.js

--- client/src/components/Team.js
+++ client/src/components/Team.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Player from "./Player";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 export const totalSearches = team => team.players
     .reduce((sum, player) => sum + player.searches, 0);
 
 const Team = ({ team }) => {
+    const [searches, setSearches] = useLocalStorage(`${team.country}-searches`, 0);
+    useEffect(() => {
+        setSearches(totalSearches(team));
+    }, [])
     return (
         <div className="team" id={team.country}>
             <div className="team-heading">
@@ -12,7 +17,7 @@ const Team = ({ team }) => {
 
                 <a className="link" href="#">[back to top]</a>
             </div>
-            <p>Total searches: {totalSearches(team)}</p>
+            <p>Total searches: {searches}</p>
             <div className="players">
                 {team.players.map(player => (
                     <Player key={player.id} player={player} />
